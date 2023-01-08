4282a9bcc0d2b53979a70d14bc39380673330d4e
guillaumeb <guillaume.bernier@klarr.ca>
2023-01-08 16:06:40 -0500

    example page updated

--- packages/circum-components-svelte/src/routes/+page.svelte
+++ packages/circum-components-svelte/src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
 	import Button from '$lib/Button.svelte';
+	import InputText from '$lib/InputText.svelte';
     import '$lib/global.css';
+
+    let textBinding = '';
 </script>
 
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Input text content => {textBinding}</p>
 <Button action="Save" />
+<InputText bind:value={textBinding} label="test"></InputText>
+<InputText bind:value={textBinding} ></InputText>
